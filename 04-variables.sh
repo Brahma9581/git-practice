f870c22c5a200b2b08845b0f5d55aadf547177d4
Brahma-Danna <brahmanayududannavarapu@gmail.com>
2024-09-01 00:02:07 +0530

    new variables added

---
+++ 04-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "please enter your user name:"
+
+read -s USERNAME   # takes input into username variable
+
+echo "your user name is: $USERNAME"
+
+echo "please enter your password:"
+
+read -s PASSWORD
+
+echo "your password is: $PASSWORD"
